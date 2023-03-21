b93aeb3a731dc1557894bff6bc095c376c82e182
Nurul30 <43957447+Nurul30@users.noreply.github.com>
2023-03-20 22:50:25 -0700

    user

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -58,7 +58,7 @@
                 <img src="{{ asset('/assets/icon/avatar.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">admin</a>
+                <a href="#" class="d-block">{{ auth()->user()->username }}</a>
             </div>
         </div>
         <div class="">
